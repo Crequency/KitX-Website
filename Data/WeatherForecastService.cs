ef1ebb576d3a9f8f1c1ff52925ee74638e120f9f
Dynesshely <catrol@qq.com>
2022-12-02 14:41:18 +0800

    🎇 Style: ...

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -1,11 +1,11 @@
-namespace KitX_Website.Data
+﻿namespace KitX_Website.Data
 {
     public class WeatherForecastService
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
