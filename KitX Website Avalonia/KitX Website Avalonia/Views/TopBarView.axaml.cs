60e5b3c449952b8927a354b4c77533bcda555108
Dynesshely <catrol@qq.com>
2022-12-04 21:25:57 +0800

    💾 Feat: WASM 真香, 启动页完工

--- KitX Website Avalonia/KitX Website Avalonia/Views/TopBarView.axaml.cs
+++ KitX Website Avalonia/KitX Website Avalonia/Views/TopBarView.axaml.cs
@@ -1,12 +1,17 @@
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
+using KitX_Website_Avalonia.ViewModels;
 
 namespace KitX_Website_Avalonia.Views
 {
     public partial class TopBarView : UserControl
     {
+        private readonly TopBarViewModel viewModel = new();
+
         public TopBarView()
         {
             InitializeComponent();
+
+            DataContext = viewModel;
         }
     }
 }
