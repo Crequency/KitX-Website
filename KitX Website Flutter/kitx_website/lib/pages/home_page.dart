6f2291f5a4174d2334de0cba590d1c3660a571b6
Dynesshely <catrol@qq.com>
2023-02-28 04:13:04 +0800

    💾 Feat: 新增下载项列表

--- KitX Website Flutter/kitx_website/lib/pages/home_page.dart
+++ KitX Website Flutter/kitx_website/lib/pages/home_page.dart
@@ -1,9 +1,9 @@
-﻿import 'package:flutter/material.dart';
+﻿import 'package:community_material_icon/community_material_icon.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'package:kitx_website/pages/controls/drawer.dart';
 import 'package:kitx_website/pages/controls/home_page_actions.dart';
-import 'package:kitx_website/utils/global.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,9 +13,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var tileRadius = ContinuousRectangleBorder(borderRadius: BorderRadius.circular(10.0));
+  var tilesPadding = 15.0;
+
   @override
   void initState() {
     super.initState();
+  }
+
+  void showItemsDialog(BuildContext context, List<Widget> items) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Container(
+          width: MediaQuery.of(context).size.width < 600 ? MediaQuery.of(context).size.width * 0.9 : 600,
+          margin: EdgeInsets.all(30),
+          child: ListView(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            children: items,
+          ),
+        ),
+      ),
+    );
+  }
+
+  void beginDownload(BuildContext context, String id) {
+    Get.back();
+    showItemsDialog(
+      context,
+      [
+        const Text('当前尚未发布, 官网下载项暂不可用, 您可前往 GitHub 仓库 Release 页面下载测试版'),
+      ],
+    );
   }
 
   @override
@@ -30,7 +60,7 @@ class _HomePageState extends State<HomePage> {
       drawer: AppDrawer(),
       body: Center(
         child: Container(
-          width: 1200,
+          width: 500,
           margin: EdgeInsets.all(30),
           child: Card(
             clipBehavior: Clip.antiAliasWithSaveLayer,
@@ -40,7 +70,7 @@ class _HomePageState extends State<HomePage> {
                 children: [
                   Container(
                     alignment: Alignment.topCenter,
-                    margin: EdgeInsets.only(top: 25),
+                    margin: EdgeInsets.only(top: 0),
                     width: 192,
                     height: 192,
                     child: InkWell(
@@ -64,15 +94,197 @@ class _HomePageState extends State<HomePage> {
                             fontSize: 50,
                           ),
                         ),
-                        Obx(
-                          () => Text(Global.versionString.value),
-                        ),
+                        Text('v3.23.04'),
+                        // Obx(
+                        //   () => Text(Global.versionString.value),
+                        // ),
                       ],
                     ),
                   ),
                   SizedBox(height: 10),
                   Divider(),
                   SizedBox(height: 10),
+                  ListView(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    children: [
+                      ListTile(
+                        title: const Text('Windows'),
+                        subtitle: const Text('Windows 10/11 supported'),
+                        shape: tileRadius,
+                        leading: const Icon(CommunityMaterialIcons.microsoft_windows),
+                        trailing: const Icon(Icons.keyboard_arrow_right),
+                        onTap: () => showItemsDialog(
+                          context,
+                          [
+                            // ListTile(
+                            //   title: const Text('适用于 Windows 64 位 (依赖 dotnet 6 运行时)'),
+                            //   subtitle: const Text('win-x64-runtime-relied.pubxml'),
+                            //   shape: tileRadius,
+                            //   trailing: const Icon(Icons.download),
+                            //   onTap: () {},
+                            // ),
+                            // SizedBox(height: tilesPadding),
+                            ListTile(
+                              title: const Text('适用于 Windows 64 位 (包含 dotnet 6 运行时)'),
+                              subtitle: const Text('win-x64-single.pubxml'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'win-x64-single.pubxml');
+                              },
+                            ),
+                            SizedBox(height: tilesPadding),
+                            ListTile(
+                              title: const Text('适用于 Windows 32 位 (包含 dotnet 6 运行时)'),
+                              subtitle: const Text('win-x86-single.pubxml'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'win-x86-single.pubxml');
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: tilesPadding),
+                      ListTile(
+                        title: const Text('GNU/Linux'),
+                        subtitle: const Text('Tested on Ubuntu 20.04+, Deepin ...'),
+                        shape: tileRadius,
+                        leading: const Icon(CommunityMaterialIcons.linux),
+                        trailing: const Icon(Icons.keyboard_arrow_right),
+                        onTap: () => showItemsDialog(
+                          context,
+                          [
+                            ListTile(
+                              title: const Text('适用于 Linux 64 位 (包含 dotnet 6 运行时)'),
+                              subtitle: const Text('linux-x64-single.pubxml'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'linux-x64-single.pubxml');
+                              },
+                            ),
+                            SizedBox(height: tilesPadding),
+                            ListTile(
+                              title: const Text('适用于 Linux arm 架构处理器 (包含 dotnet 6 运行时)'),
+                              subtitle: const Text('linux-arm-single.pubxml'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'linux-arm-single.pubxml');
+                              },
+                            ),
+                            SizedBox(height: tilesPadding),
+                            ListTile(
+                              title: const Text('适用于 Linux arm 架构 64 位处理器 (包含 dotnet 6 运行时)'),
+                              subtitle: const Text('linux-arm64-single.pubxml'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'linux-arm64-single.pubxml');
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: tilesPadding),
+                      ListTile(
+                        title: const Text('MacOS'),
+                        subtitle: const Text('Tested on MacOS Monterey'),
+                        shape: tileRadius,
+                        leading: const Icon(CommunityMaterialIcons.apple),
+                        trailing: const Icon(Icons.keyboard_arrow_right),
+                        onTap: () => showItemsDialog(
+                          context,
+                          [
+                            ListTile(
+                              title: const Text('适用于 MacOS 64 位 (Apple 芯片) (包含 dotnet 6 运行时)'),
+                              subtitle: const Text('osx-arm64-single.pubxml'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'osx-arm64-single.pubxml');
+                              },
+                            ),
+                            SizedBox(height: tilesPadding),
+                            ListTile(
+                              title: const Text('适用于 MacOS 64 位 (Intel 芯片) (包含 dotnet 6 运行时)'),
+                              subtitle: const Text('osx-x64-single.pubxml'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'osx-x64-single.pubxml');
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: tilesPadding),
+                      ListTile(
+                        title: const Text('Android'),
+                        subtitle: const Text('Android 5.0+ supported'),
+                        shape: tileRadius,
+                        leading: const Icon(CommunityMaterialIcons.android),
+                        trailing: const Icon(Icons.keyboard_arrow_right),
+                        onTap: () => showItemsDialog(
+                          context,
+                          [
+                            ListTile(
+                              title: const Text('适用于 Android (包含多个架构支持)'),
+                              subtitle: const Text('kitx-mobile-release.apk'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'kitx-mobile-release.apk');
+                              },
+                            ),
+                            SizedBox(height: tilesPadding),
+                            ListTile(
+                              title: const Text('适用于 Android arm 架构 64 位处理器 (ABIv8)'),
+                              subtitle: const Text('kitx-mobile-arm64-v8a-release.apk'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'kitx-mobile-arm64-v8a-release.apk');
+                              },
+                            ),
+                            SizedBox(height: tilesPadding),
+                            ListTile(
+                              title: const Text('适用于 Android arm 架构处理器 (ABIv7)'),
+                              subtitle: const Text('kitx-mobile-armeabi-v7a-release.apk'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'kitx-mobile-armeabi-v7a-release.apk');
+                              },
+                            ),
+                            SizedBox(height: tilesPadding),
+                            ListTile(
+                              title: const Text('适用于 Android x86 架构处理器 64 位'),
+                              subtitle: const Text('kitx-mobile-x86_64-release.apk'),
+                              shape: tileRadius,
+                              trailing: const Icon(Icons.download),
+                              onTap: () {
+                                beginDownload(context, 'kitx-mobile-x86_64-release.apk');
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: tilesPadding),
+                      ListTile(
+                        enabled: false,
+                        title: const Text('iOS'),
+                        subtitle: const Text('No test yet'),
+                        shape: tileRadius,
+                        leading: const Icon(CommunityMaterialIcons.apple_ios),
+                        trailing: const Icon(Icons.keyboard_arrow_right),
+                        onTap: () {},
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
