472853d0daf33b5bb1b6f91730a4704cadd7e845
Dynesshely <catrol@qq.com>
2023-03-27 19:16:14 +0800

    📝 Chore: Deploy scripts.

---
+++ KitX Website Flutter/kitx_website/deploy.ps1
@@ -0,0 +1,9 @@
+﻿
+# Clean built files
+flutter clean
+
+# Build with web profile
+flutter build web --web-renderer html --release
+
+# Run optimizer to reduce the size of the built files
+flutter pub run flutter_web_optimizer optimize --asset-base https://kitx.apps.catrol.cn/
