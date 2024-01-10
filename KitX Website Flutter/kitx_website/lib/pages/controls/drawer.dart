06bde09328c4a731625e789b72b6a8b3e57c642b
Dynesshely <catrol@qq.com>
2024-01-11 01:36:02 +0800

    💾 Feat(DocsPage): Optimized.

--- KitX Website Flutter/kitx_website/lib/pages/controls/drawer.dart
+++ KitX Website Flutter/kitx_website/lib/pages/controls/drawer.dart
@@ -15,22 +15,22 @@ class AppDrawer extends StatelessWidget {
         children: <Widget>[
           ListTile(title: const Text(' ')),
           ListTile(
-            leading: Icon(Icons.home),
+            leading: const Icon(Icons.home),
             title: Text('Home_Page'.tr),
             onTap: () => Global.navPageTo(0),
           ),
           ListTile(
-            leading: Icon(Icons.account_balance_rounded),
+            leading: const Icon(Icons.account_balance_rounded),
             title: Text('Plugins_Page'.tr),
             onTap: () => Global.navPageTo(1),
           ),
           ListTile(
-            leading: Icon(CommunityMaterialIcons.file_document),
+            leading: const Icon(CommunityMaterialIcons.file_document),
             title: Text('Public_Docs'.tr),
             onTap: () => Global.navPageTo(2),
           ),
           ListTile(
-            leading: Icon(CommunityMaterialIcons.github),
+            leading: const Icon(CommunityMaterialIcons.github),
             title: const Text('GitHub'),
             onTap: () => openLink('GitHubRepo_KitX'),
           ),
--- KitX Website Flutter/kitx_website/lib/pages/docs_page.dart
+++ KitX Website Flutter/kitx_website/lib/pages/docs_page.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 import 'dart:ui_web';
 
 import 'package:flutter/material.dart';
+import 'package:kitx_website/utils/global.dart';
 
 class DocsPage extends StatefulWidget {
   @override
@@ -24,12 +25,39 @@ class _DocsPageState extends State<DocsPage> {
         ..style.border = 'none';
     });
 
-    return SizedBox(
-      width: double.infinity,
-      height: double.infinity,
-      child: HtmlElementView(
-        viewType: 'iframe-webview',
-        onPlatformViewCreated: (int id) {},
+    const actionsPadding = SizedBox(width: 8.0);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Docs Inside'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.home),
+            onPressed: () => Global.navPageTo(0),
+          ),
+          actionsPadding,
+          IconButton(
+            icon: const Icon(Icons.account_balance_rounded),
+            onPressed: () => Global.navPageTo(1),
+          ),
+          actionsPadding,
+          IconButton(
+            icon: const Icon(Icons.open_in_new),
+            onPressed: () {
+              window.open('https://kitx.docs.catrol.cn/', 'KitX Docs');
+            },
+          ),
+          actionsPadding,
+        ],
+      ),
+      drawerEnableOpenDragGesture: false,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: HtmlElementView(
+          viewType: 'iframe-webview',
+          onPlatformViewCreated: (int id) {},
+        ),
       ),
     );
   }
