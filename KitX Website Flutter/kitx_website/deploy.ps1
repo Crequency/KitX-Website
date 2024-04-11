33f5440c6c9e657b4c68408d006b56d358d3aae1
Dynesshely <catrol@qq.com>
2024-04-11 17:59:18 +0800

    📝 Chore(Scripts): Optimized deploy script

--- KitX Website Flutter/kitx_website/deploy.ps1
+++ KitX Website Flutter/kitx_website/deploy.ps1
@@ -7,8 +7,6 @@ flutter build web --release --web-renderer canvaskit --tree-shake-icons --no-web
 ## Run optimizer to reduce the size of the built files
 # flutter pub run flutter_web_optimizer optimize --asset-base https://kitx.apps.catrol.cn/
 
-Remove-Item -r dist
-
 mkdir dist
 
 Copy-Item -r ./build/web/* dist/
@@ -21,11 +19,11 @@ git add dist
 
 git stash
 
-Remove-Item -r dist -Recurse -Force
+Remove-Item -r dist
 
 git checkout deploy
 
-Remove-Item -r dist -Recurse -Force
+Remove-Item -r dist
 
 git add .
 
