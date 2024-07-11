511b89b5d2fa847e1a12e5c8800e969d807cb6c4
Dynesshely <catrol@qq.com>
2024-07-12 03:26:02 +0800

    💾 Feat: Import timeline widgets library

--- KitX Website Flutter/kitx_website/lib/pages/milestone_page.dart
+++ KitX Website Flutter/kitx_website/lib/pages/milestone_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timelines/timelines.dart';
 
 class MilestonesPage extends StatefulWidget {
   const MilestonesPage({super.key});
@@ -15,6 +16,15 @@ class _MilestonesPageState extends State<MilestonesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Under developing ...'));
+    return Timeline.tileBuilder(
+      builder: TimelineTileBuilder.fromStyle(
+        contentsAlign: ContentsAlign.alternating,
+        contentsBuilder: (context, index) => Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Text('Timeline Event $index'),
+        ),
+        itemCount: 10,
+      ),
+    );
   }
 }
