c7d6ac9d6bd2bd8200d4425659ce26fd61a81c4c
Dynesshely <catrol@qq.com>
2024-08-09 11:21:16 +0800

    📝 Chore: Split build scripts

--- KitX Website Flutter/kitx_website/build.ps1
+++ KitX Website Flutter/kitx_website/build.ps1
@@ -1,2 +1,20 @@
-﻿flutter clean
-flutter build web --release --web-renderer canvaskit --tree-shake-icons --no-web-resources-cdn
+﻿## Clean built files
+flutter clean
+
+## Build with web profile
+flutter build web --release --web-renderer canvaskit --no-tree-shake-icons --no-web-resources-cdn
+
+## Run optimizer to reduce the size of the built files
+# flutter pub run flutter_web_optimizer optimize --asset-base https://kitx.apps.catrol.cn/
+
+mkdir dist
+
+Copy-Item -r ./build/web/* dist/
+
+Move-Item dist ../../
+
+Set-Location ../../
+
+tar -cf dist.tar dist
+
+Remove-Item -r dist
--- KitX Website Flutter/kitx_website/deploy.ps1
+++ KitX Website Flutter/kitx_website/deploy.ps1
@@ -1,23 +1,4 @@
-﻿## Clean built files
-flutter clean
-
-## Build with web profile
-flutter build web --release --web-renderer canvaskit --no-tree-shake-icons --no-web-resources-cdn
-
-## Run optimizer to reduce the size of the built files
-# flutter pub run flutter_web_optimizer optimize --asset-base https://kitx.apps.catrol.cn/
-
-mkdir dist
-
-Copy-Item -r ./build/web/* dist/
-
-Move-Item dist ../../
-
-Set-Location ../../
-
-tar -cf dist.tar dist
-
-Remove-Item -r dist
+﻿& build.ps1
 
 git add dist.tar
 
