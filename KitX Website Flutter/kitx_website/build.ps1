a4ea30d4adf969bc55e374081c2c66ee1eb0f459
Dynesshely <catrol@qq.com>
2024-09-09 03:15:40 +0800

    📝 Chore: Let gh actions to use build scripts

--- KitX Website Flutter/kitx_website/build.ps1
+++ KitX Website Flutter/kitx_website/build.ps1
@@ -1,11 +1,15 @@
-﻿## Clean built files
+﻿param(
+    [string]$baseHref = "/"
+)
+
+## Clean built files
 flutter clean
 
 ## Make sure custom build scripts are executed
 dart run build_runner build --delete-conflicting-outputs
 
 ## Build with web profile
-flutter build web --release --web-renderer canvaskit --no-tree-shake-icons --no-web-resources-cdn
+flutter build web --release --web-renderer canvaskit --no-tree-shake-icons --no-web-resources-cdn --base-href "$baseHref"
 
 ## Run optimizer to reduce the size of the built files
 # flutter pub run flutter_web_optimizer optimize --asset-base https://kitx.apps.catrol.cn/
