7a7c36fa1253f65822ae0cd2e1da22c7769f2151
Dynesshely <catrol@qq.com>
2024-09-18 10:58:24 +0800

    💾 Feat: Ready to write backend system

---
+++ KitX Website Backend/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace KitX_Website_Backend;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
