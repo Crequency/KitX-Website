0615941ac539ff115d93ca1245f736407e28e977
Dynesshely <catrol@qq.com>
2025-02-26 04:04:49 +0800

    📝 Chore(CI): Updated build script for new flutter editions

--- KitX Website Flutter/kitx_website/build.ps1
+++ KitX Website Flutter/kitx_website/build.ps1
@@ -9,7 +9,7 @@ flutter clean
 dart run build_runner build --delete-conflicting-outputs
 
 ## Build with web profile
-flutter build web --release --web-renderer canvaskit --no-tree-shake-icons --no-web-resources-cdn --base-href "$baseHref"
+flutter build web --release --wasm --no-tree-shake-icons --no-web-resources-cdn --base-href "$baseHref"
 
 ## Run optimizer to reduce the size of the built files
 # flutter pub run flutter_web_optimizer optimize --asset-base https://kitx.apps.catrol.cn/
--- KitX Website Flutter/kitx_website/lib/common/build/app_build_timestamp.g.dart
+++ KitX Website Flutter/kitx_website/lib/common/build/app_build_timestamp.g.dart
@@ -1,4 +1,4 @@
 // Generated file on build.
 
 /// Timestamp that records last build of the app. Can be shown in the app.
-const int lastAppBuildTimestamp = 1725820249737;
+const int lastAppBuildTimestamp = 1740513713297;
