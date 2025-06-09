495bb538d1ba281a758a96a35f054a62fdfdddbe
Dynesshely <catrol@qq.com>
2025-06-09 14:41:54 +0800

    📝 Chore(Deploy): Allow bypass build, which only update server

--- KitX Website Flutter/kitx_website/deploy.ps1
+++ KitX Website Flutter/kitx_website/deploy.ps1
@@ -5,25 +5,25 @@
 
 if (-not $BypassBuild) {
     . $PSScriptRoot/build.ps1
+    git add dist.tar
+    git stash
+    git checkout deploy
+
+    Remove-Item dist.tar
+
+    git add .
+    git commit -m "Clean old files"
+    git stash pop
+    git add .
+    git commit -m "Deploy new files"
+    git push
+    git checkout dev=main
+
+    Write-Output ""
+    Write-Output "Press any key to update server ..."
+
+    [void][System.Console]::ReadKey($true)
 }
-git add dist.tar
-git stash
-git checkout deploy
-
-Remove-Item dist.tar
-
-git add .
-git commit -m "Clean old files"
-git stash pop
-git add .
-git commit -m "Deploy new files"
-git push
-git checkout dev=main
-
-Write-Output ""
-Write-Output "Press any key to update server ..."
-
-[void][System.Console]::ReadKey($true)
 
 ssh git@git.catrol.cn -t "sudo bash -c ~/maintain/update-kitx.apps.catrol.cn.sh"
 
