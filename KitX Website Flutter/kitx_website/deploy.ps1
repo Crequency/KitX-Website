db1bf6af2ca72402fe674762f98c34ef1f78a788
Dynesshely <catrol@qq.com>
2025-06-09 14:15:16 +0800

    📝 Chore(Deploy): Updated deploy pipeline

--- KitX Website Flutter/kitx_website/deploy.ps1
+++ KitX Website Flutter/kitx_website/deploy.ps1
@@ -1,25 +1,16 @@
 ﻿& build.ps1
-
 git add dist.tar
-
 git stash
-
 git checkout deploy
 
 Remove-Item dist.tar
 
 git add .
-
 git commit -m "Clean old files"
-
 git stash pop
-
 git add .
-
 git commit -m "Deploy new files"
-
 git push
-
 git checkout dev=main
 
 Write-Output ""
@@ -27,7 +18,7 @@ Write-Output "Press any key to update server ..."
 
 [void][System.Console]::ReadKey($true)
 
-ssh catrol@catrol.cn -t "cd /opt/1panel/apps/openresty/openresty/www/sites/kitx.apps.catrol.cn/index; sudo git checkout deploy; sudo git pull; sudo tar -xf dist.tar"
+ssh git@git.catrol.cn -t "sudo bash -c ~/maintain/update-kitx.apps.catrol.cn.sh"
 
 Write-Output ""
 Write-Output "Press any key to continue ..."
