62732801346602adccc30a0bcd3677575dcba7f4
Dynesshely <catrol@qq.com>
2025-06-10 14:34:59 +0800

    📝 Chore: Updated build time

--- KitX Website Flutter/kitx_website/lib/common/build/app_build_timestamp.g.dart
+++ KitX Website Flutter/kitx_website/lib/common/build/app_build_timestamp.g.dart
@@ -1,4 +1,4 @@
 // Generated file on build.
 
 /// Timestamp that records last build of the app. Can be shown in the app.
-const int lastAppBuildTimestamp = 1749463946126;
+const int lastAppBuildTimestamp = 1749537201485;
