7930e11940c9fc60dcc0b72081c5bbd6a5f88d0b
Dynesshely <catrol@qq.com>
2025-06-10 17:14:49 +0800

    🔧 📝 Fix, Chore(Deploy): Usage of git mistake

--- KitX Website Flutter/kitx_website/build.ps1
+++ KitX Website Flutter/kitx_website/build.ps1
@@ -8,17 +8,17 @@ flutter clean
 ## Make sure custom build scripts are executed
 dart run build_runner build --delete-conflicting-outputs
 
+## Update build time
+git add lib/common/build/app_build_timestamp.g.dart
+git commit -m "📝 Chore: Updated build runner results"
+
 ## Build with web profile
 flutter build web --release --wasm --no-tree-shake-icons --no-web-resources-cdn --base-href "$baseHref"
 
 ## Run optimizer to reduce the size of the built files
 # flutter pub run flutter_web_optimizer optimize --asset-base https://kitx.apps.catrol.cn/
 
-git add lib/common/build/app_build_timestamp.g.dart
-git commit -m "📝 Chore: Updated build time"
-
 mkdir dist
 
 Copy-Item -r ./build/web/* dist/
-
 Move-Item dist ../../../
--- KitX Website Flutter/kitx_website/deploy.ps1
+++ KitX Website Flutter/kitx_website/deploy.ps1
@@ -1,6 +1,8 @@
 ﻿param (
     [Parameter()]
-    [switch]$BypassBuild = $false
+    [switch]$BypassBuild = $false,
+    [Parameter()]
+    [switch]$UseBuildCache = $false
 )
 
 function Save-GitBranch {
@@ -8,28 +10,65 @@ function Save-GitBranch {
     return $currentBranch
 }
 
-if (-not $BypassBuild) {
+if ($BypassBuild -and $UseBuildCache) {
+    Write-Output ">>> You can not bypass build and use build cache at the same time."
+    exit 1
+}
+
+if (-not $BypassBuild -and -not $UseBuildCache) {
     Write-Output ">>> Current changes stashed"
     git add .
     git stash
     $lastBranch = Save-GitBranch
     Write-Output ">>> Building ..."
-    . $PSScriptRoot/build.ps1
+    Push-Location $PSScriptRoot
+    ./build.ps1
     Write-Output ">>> Committing ..."
     git checkout deploy
-    Move-Item ../../../dist ./
+    Push-Location ../../
+    if (Test-Path -Path dist) {
+        Remove-Item -Recurse -Force dist
+    }
+    Move-Item ../dist ./
     git add ./dist -f
     git commit -m "Deploy new files"
     git push server --all
+    Pop-Location
+    Pop-Location
+    Write-Output ">>> Restore stashed changes ..."
     git checkout $lastBranch
-    if (Test-Path dist) {
-        Remove-Item dist -Recurse -Force
-    }
+    git stash pop
 
     Write-Output ""
     Write-Output "Press any key to update server ..."
 
     [void][System.Console]::ReadKey($true)
+}
+
+if ($UseBuildCache -and -not $BypassBuild) {
+    Write-Output ">>> Current changes stashed"
+    git add .
+    git stash
+    $lastBranch = Save-GitBranch
+    Write-Output ">>> Using build cache ..."
+    Push-Location $PSScriptRoot
+    New-Item -Path ./dist -ItemType Directory -Force | Out-Null
+    Copy-Item ./build/web/* ./dist -Recurse -Force
+    Move-Item ./dist ../../../ -Force
+    Push-Location ../../
+    git checkout deploy
+    if (Test-Path -Path dist) {
+        Remove-Item -Recurse -Force dist
+    }
+    Move-Item ../dist ./ -Force
+    git add ./dist -f
+    git commit -m "Deploy new files"
+    git push server --all
+    Pop-Location
+    Pop-Location
+    Write-Output ">>> Restore stashed changes ..."
+    git checkout $lastBranch
+    git stash pop
 }
 
 ssh git@git.catrol.cn -t "sudo bash -c ~/maintain/update-kitx.apps.catrol.cn.sh"
